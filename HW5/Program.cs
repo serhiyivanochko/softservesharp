5094a58086a5878fd271eb7b6ea0bb9080263592
Serhii Ivanochko <sivanotc@training.local>
2018-09-07 18:28:02 +0300

    HW6

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -6,52 +6,146 @@ using System.Threading.Tasks;
 
 namespace HW5
 {
+    #region Task 1
     interface IFlyable {
         void Fly();
     }
     class Bird : IFlyable {
         
         string Name { get; set; }
-        public Bird(string name) {
+        bool canFly { get; set; }
+        public Bird(string name, bool canfly) {
             this.Name = name;
-        }
-        void canFly() {
-            Console.WriteLine("BirdFly");
+            this.canFly = canfly;
         }
         void IFlyable.Fly()
         {
-            canFly();
+            if (this.canFly)
+            {
+                Console.WriteLine("Bird can fly");
+            }
+            else {
+                Console.WriteLine("Bird can not fly");
+            }
+            
         }
     }
     class Plane : IFlyable {
         string Mark { get; set; }
-        public Plane(string mark) {
+        bool highFly { get; set; }
+        public Plane(string mark, bool highfly) {
             this.Mark = mark;
+            this.highFly = highfly;
         }
-        void highFly() {
-            Console.WriteLine("PlaneFly");
-        }
+       
         void IFlyable.Fly()
         {
-            highFly();
+            if (this.highFly)
+            {
+                Console.WriteLine("Plane can not fly");
+            }
+            else
+            {
+                Console.WriteLine("Plane fly");
+            }
         }
     }
+    #endregion
+
+    #region HomeWork
+    interface IDeveloper {
+        int Tool { get; set; }
+        void Create();
+        void Destoy();
+    }
+    class Programmer : IDeveloper
+    {
+        public int Tool { get; set; }
+        public Programmer(int tool) {
+            this.Tool = tool;
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("Programmer {0} Created", this.Tool);
+        }
+
+        public void Destoy()
+        {
+            Console.WriteLine("Programmer {0} Destroyed", this.Tool);
+        }
+    }
+    class Builder : IDeveloper
+    {
+        public int Tool { get; set; }
+        public Builder(int tool) {
+            this.Tool = tool;
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("Builder {0} Created", Tool);
+        }
+
+        public void Destoy()
+        {
+            Console.WriteLine("Builder {0} Destroyed", Tool);
+        }
+    }
+    #endregion
+
     class Program
     {
         static void Main(string[] args)
         {
-            List<IFlyable> list = new List<IFlyable>();
-            list.Add(new Bird("Bird1"));
-            list.Add(new Plane("Plane1"));
-            list.Add(new Bird("Bird2"));
-            list.Add(new Plane("Plane2"));
-            list.Add(new Bird("Bird3"));
-            list.Add(new Plane("Plane3"));
+            #region Task 1
+            //List<IFlyable> list = new List<IFlyable>();
+            //list.Add(new Bird("Bird1",true));
+            //list.Add(new Plane("Plane1", true));
+            //list.Add(new Bird("Bird2",false));
+            //list.Add(new Plane("Plane2",false));
+            //list.Add(new Bird("Bird3", true));
+            //list.Add(new Plane("Plane3",false));
+
+            //foreach (var current in list) {
+            //    current.Fly();
+            //}
+            #endregion
+
+            #region HomeWork
+            List<IDeveloper> list = new List<IDeveloper>();
+            list.Add(new Builder(1));
+            list.Add(new Programmer(2));
+            list.Add(new Builder(3));
+            list.Add(new Programmer(4));
+            list.Add(new Builder(5));
+            list.Add(new Programmer(6));
+            list.Add(new Builder(7));
+            list.Add(new Programmer(8));
 
             foreach (var current in list) {
-                current.Fly();
+                current.Create();
+                current.Destoy();
+            }
+            Dictionary<uint, string> dic = new Dictionary<uint, string>();
+            for (int i = 0; i < 7; i++) {
+                Console.WriteLine("Enter {0} user ID: ", i + 1);
+                uint ID = Convert.ToUInt32(Console.ReadLine());
+                Console.WriteLine("Enter {0} user Name: ", i + 1);
+                string name = Console.ReadLine();
+                dic.Add(ID, name);
+            }
+            Console.WriteLine("Enter user ID: ");
+            uint fID = Convert.ToUInt32(Console.ReadLine());
+            if (dic[fID] != null)
+            {
+                Console.WriteLine(dic[fID]);
+            }
+            else {
+                Console.WriteLine("Can not find user with current ID");
             }
 
+            #endregion
         }
     }
 }
---
+++ HW6/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    class Program
+    {
+        #region Task 1
+        static double Div(double num1, double num2) {
+            try
+            {
+                return num1 / num2;
+            }
+            catch (DivideByZeroException e) {
+                return e.HResult;
+            }
+            
+        }
+        #endregion
+
+        #region Task2
+
+        #endregion
+
+        #region Task3
+        static void ReadFolder(List<string> list, DirectoryInfo dir)
+        {
+
+
+            DirectoryInfo[] curr_dir = dir.GetDirectories();
+            FileInfo[] curr_files = dir.GetFiles();
+            list.Add(dir.Name + " Folder");
+            foreach (var current in curr_dir)
+            {
+                try
+                {
+                    ReadFolder(list, current);
+                }
+                catch
+                {
+                }
+            }
+            foreach (var current in curr_files)
+            {
+                list.Add(current.Name + " File " + current.Length);
+            }
+
+        }
+        #endregion
+
+        #region Task4
+
+        #endregion
+        static void Main(string[] args)
+        {
+            #region Task 1
+            double num1 = 0.5, num2 = 0;
+            Console.WriteLine(Div(num1, num2));
+            #endregion
+
+            #region Task2
+            StreamReader sr = new StreamReader("data.txt");
+            StreamWriter sw = new StreamWriter("rez.txt");
+
+            string str;
+            try
+            {
+                while ((str = sr.ReadLine()) != null)
+                {
+                    sw.WriteLine(str);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                sr.Close();
+                sw.Close();
+            }
+            try
+            {
+                File.WriteAllText("rez.txt", File.ReadAllText("data.txt"));
+            }
+            catch {
+                Console.WriteLine("Error");
+            }
+            #endregion
+
+            #region Task3
+            DirectoryInfo dir = new DirectoryInfo(@"C:\");
+            List<string> list = new List<string>();
+            ReadFolder(list, dir);
+            StreamWriter sw1 = new StreamWriter("DirectoryC.txt");
+            foreach (var current in list) {
+                try
+                {
+                    sw1.WriteLine(current);
+                }
+                catch { }
+            }
+            sw1.Close();
+
+
+
+            #endregion
+
+            #region Task4
+            string sourceDir = @"D:\";
+            string[] txtList = null;
+            try
+            {
+                txtList = Directory.GetFiles(sourceDir, "*.txt");
+            }
+            catch { }
+            foreach (var current in txtList) {
+                Console.WriteLine(File.ReadAllText(current));
+            }
+            #endregion
+        }
+    }
+}
