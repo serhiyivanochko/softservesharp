e6226d09a005697d87e6fb2a467d36d6bd2d9503
Second <second@mail.com>
2018-09-11 11:31:18 +0300

    Add presentation

---
+++ Presentation/Delegate/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        delegate void DelegateEx();
+
+        public static void MethodA()
+        {
+            Console.WriteLine("MethodA");
+        }
+        public static void MethodB()
+        {
+            Console.WriteLine("MethodB");
+        }
+        public static void MethodC()
+        {
+            Console.WriteLine("MethodC");
+        }
+   
+        static void Main(string[] args)
+        {
+            
+            DelegateEx DEx = new DelegateEx(MethodA);
+            DEx();
+            
+            DEx = MethodB;
+            DEx();
+
+            DEx = new DelegateEx(MethodC);
+            DEx();
+
+
+            Console.WriteLine("\n-----Group-----\n");
+
+            DelegateEx MA = MethodA;
+            DelegateEx MB = MethodB;
+            DelegateEx MC = MethodC;
+            
+           
+            DEx = MA; 
+            DEx = DEx + MC; 
+            DEx += MB; // MethodA->MethodC->MethodB
+            DEx();
+            Console.WriteLine();
+
+            DEx -= MB; // MethodA->MethodC
+            DEx();// MethodC
+            Console.WriteLine();
+
+            DEx += MA; // MethodA->MethodC->MethodA
+            DEx(); // MethodA
+            Console.WriteLine();
+
+            DEx -= MA; // MethodA->MethodC
+            DEx(); // MethodC
+            Console.WriteLine();
+
+            DEx -= MA; // MethodC
+            DEx();
+            Console.WriteLine();
+
+            DEx -= MC; // DEx => Empty
+
+
+        }
+
+    }
+}
---
+++ Presentation/Event/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    class Program
+    {
+        public static event EventHandler MyEvent
+        {
+            add
+            {
+                Console.WriteLine("Add operation");
+            }
+
+            remove
+            {
+                Console.WriteLine("Remove operation");
+            }
+        }
+
+
+        delegate void DelegateEx();
+
+        static event DelegateEx myEvent;
+
+        public static void MethodA()
+        {
+            Console.WriteLine("MethodA");
+        }
+        public static void MethodB()
+        {
+            Console.WriteLine("MethodB");
+        }
+        public static void MethodC()
+        {
+            Console.WriteLine("MethodC");
+        }
+        
+
+
+        static void Main(string[] args)
+        {
+
+         
+
+
+            MyEvent += new EventHandler(DoNothing);
+            MyEvent -= null;
+
+
+
+            myEvent = MethodA;
+            myEvent += MethodB;
+            myEvent += MethodC;
+
+            Console.WriteLine();
+            myEvent();
+
+            myEvent -= MethodA;
+            myEvent += MethodB;
+
+
+            Console.WriteLine();
+            myEvent();
+
+
+
+
+
+        }
+
+        private static void DoNothing(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
