769ecc9977272e115e99a67537b0935bbaccbec8
sivanotc <sivanotc@CLASS42>
2018-09-13 19:44:29 +0300

    "HW10 demo"

---
+++ HW10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW10
+{
+    public delegate void MyDel(int m);
+
+    class Student {
+        public string name { get; set; }
+        public List<int> list { get; set; }
+        public event MyDel MyEvent;
+        public void AddMark(int mark) {
+            list.Add(mark);
+
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
